1e3aaa281efbfd4b5827ad4e215b3e1a0c83931f
Vivek1325 <kvivek2001@gmail.com>
2023-12-29 08:32:21 +0530

    added 01 and 02 of basics

---
+++ 01-basics/01_let_const_var.js
@@ -0,0 +1,11 @@
+const user_name = 'Vivek'
+
+var id = 12 // do not use as it does not follow scope rules, it has issues with block and functional scope
+
+// use let instead
+
+let i = 0;
+
+accountId = 1223 // this also works
+
+console.table([user_name, id, i, accountId])
---
+++ 01-basics/02_data_types.js
@@ -0,0 +1,19 @@
+// number 
+let i = 0
+
+//string
+let my_name = "Vivek"
+
+// boolean
+let isMale = true
+
+// null = standalone value and it's type is object
+// undefined = has not been given value yet and it's type is undefined itself
+let temperature = null
+
+let game;
+console.log(game);  
+console.log(typeof game);
+console.log(typeof temperature);
+
+// symbol = used to identify unique componenets in reactJs etc.
